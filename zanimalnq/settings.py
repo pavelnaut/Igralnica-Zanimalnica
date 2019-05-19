1d87179ba1515be685a9dfcbb8cb901be7bf9d4d
Bad-Wolf <pavelmbogdanov@gmail.com>
2019-05-19 19:39:34 +0300

    Application form that sends emails

--- zanimalnq/settings.py
+++ zanimalnq/settings.py
@@ -63,13 +63,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'igralnicatest@gmail.com'
-EMAIL_HOST_PASSWORD = 'parolataepassword123'
-
 
 ROOT_URLCONF = 'zanimalnq.urls'
 
